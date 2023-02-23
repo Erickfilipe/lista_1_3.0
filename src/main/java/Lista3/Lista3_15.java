484e2cb038ee4f92aafd250a9139f5c943198116
erickbraga <erickfelipedeandrade33@gmail.com>
2023-02-22 22:11:54 -0300

    Merge remote-tracking branch 'origin/main' lista 3 concluida
    # Conflicts:
    #	.idea/workspace.xml

--- src/main/java/Lista3/Lista3_15.java
+++ src/main/java/Lista3/Lista3_15.java
@@ -2,6 +2,19 @@ package Lista3;
 
 public class Lista3_15 {
     public static void main(String[] args) {
+        double valorAtual = 1;
+        double valoranterior;
+        System.out.println("Quantida de trigo que se pode colocar em tabuleiro: ");
+        double soma = 0;
+
+        for (double i = 1; i < 65; i++) {
+            System.out.println(valorAtual);
+            valoranterior = valorAtual;
+            valorAtual = valoranterior * 2;
+            soma = soma + valoranterior;
+        }
+        System.out.println(soma);
+
 
     }
 }
